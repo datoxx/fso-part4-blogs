b4ac1d91b9457b848fe06854c6c8a21f84b5ca38
datoxx <davit.ardzenadze.05@gmail.com>
2022-01-22 21:01:12 +0400

    4.9, test blogs identifer property

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -3,8 +3,31 @@
 const  mongoose  = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 test('blog are returned as json', async() => {
   await api
@@ -13,11 +36,18 @@ test('blog are returned as json', async() => {
     .expect('Content-Type', /application\/json/)
 }, 100000)
 
+test('blog id ', async() => {
+  const response = await api.get('/api/blogs')
+
+  const identId = response.body.map(item => item.id)
+  expect(identId).toBeDefined()
+})
+
 
 
 test('amount of blog posts', async() => {
   const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(0)
+  expect(response.body).toHaveLength(initialBlogs.length)
 })
 
 afterAll(() => {

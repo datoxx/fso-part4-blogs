de2bde8519ab39ada639ec0b8065e60f67a17701
datoxx <davit.ardzenadze.05@gmail.com>
2022-02-13 21:27:39 +0400

    4.19

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 /* eslint-disable linebreak-style */
-const config = require('./utils/config')
-const express = require('express')
 require('express-async-errors')
-const app = express()
+
+const express = require('express')
 const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const usersRouter = require('./controllers/users')
+const mongoose = require('mongoose')
+
+
+const config = require('./utils/config')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
+//models
+//const User = require('./models/user');
+//const Blog = require('./models/blog');
+
+const app = express()
+
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -27,6 +38,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter )
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,10 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
-
 
 
 
@@ -11,6 +13,16 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
@@ -18,7 +30,15 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).json({error: 'blog is invalid'})
   } 
 
-  const user = await User.findOne({})
+
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if(!decodedToken.id) {
+    return response.status(401).json({error: 'token missing or invalid'})
+  }
+
+  const user = await User.findById(decodedToken.id)
+  
 
   const blog = new Blog({
     title: body.title,
@@ -35,6 +55,7 @@ blogsRouter.post('/', async (request, response) => {
 
   response.status(201).json(saveBlog)
 })
+
 
 blogsRouter.get('/:id', async(request, response) => {
   const blog = await Blog.findById(request.params.id)
